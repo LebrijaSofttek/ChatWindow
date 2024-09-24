68fe0ff65fbfbb85cbcff59d7a6fea6b6ba5312f
LebrijaSofttek <juan.lebrija@softtek.com>
2024-09-24 13:08:55 -0600

    Initial Commit

---
+++ ChatWindow/ChatWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWindowApp.swift
+//  ChatWindow
+//
+//  Created by Godin on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
